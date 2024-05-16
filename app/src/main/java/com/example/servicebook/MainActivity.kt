2416f59cbb2ed084f4408034b38b9a6aff0536dd
siwexman <karol.straczek@gmail.com>
2024-05-16 23:21:50 +0200

    Starting

--- app/src/main/java/com/example/servicebook/MainActivity.kt
+++ app/src/main/java/com/example/servicebook/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.servicebook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.servicebook.ui.theme.ServiceBookTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +31,9 @@ class MainActivity : ComponentActivity() {
             ServiceBookTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ServiceBookPreview()
                 }
             }
         }
@@ -30,17 +41,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopAppBar(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.padding(15.dp, 10.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.service_book),
+                contentDescription = null,
+                modifier = Modifier,
+            )
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displaySmall,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+    }
 }
+
+@Composable
+fun CarItem(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Text(text = "Car Name")
+        Row(
+            horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 10.dp)
+        ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Active")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Repairs")
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ServiceBookPreview() {
     ServiceBookTheme {
-        Greeting("Android")
+        CarItem()
     }
 }
