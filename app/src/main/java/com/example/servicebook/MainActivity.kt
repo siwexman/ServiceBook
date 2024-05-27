082354625d9fa74efd9bbb97601e41537a1e0f75
siwexman <karol.straczek@gmail.com>
2024-05-27 19:07:14 +0200

    Added animation

--- app/src/main/java/com/example/servicebook/MainActivity.kt
+++ app/src/main/java/com/example/servicebook/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.servicebook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,25 +15,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CornerBasedShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.servicebook.ui.theme.ServiceBookTheme
 import com.example.servicebook.ui.theme.Shapes
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +51,8 @@ class MainActivity : ComponentActivity() {
             ServiceBookTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     ServiceBookPreview()
                 }
@@ -78,6 +91,8 @@ fun TopAppBar(shape: CornerBasedShape, modifier: Modifier = Modifier) {
 
 @Composable
 fun CarItem(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+
     Card(
         shape = Shapes.medium,
         modifier = Modifier.padding(10.dp)
@@ -85,31 +100,30 @@ fun CarItem(modifier: Modifier = Modifier) {
         Column(
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMedium
+                )
+            )
         ) {
             Text(
                 text = "Car Name",
                 style = MaterialTheme.typography.titleLarge,
                 modifier = Modifier.padding(5.dp)
             )
-            Row(
-                horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(0.dp, 10.dp)
-            ) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(
-                        text = "Active",
-                        style = MaterialTheme.typography.labelSmall
-                    )
-                }
-                Button(onClick = { /*TODO*/ }) {
-                    Text(
-                        text = "Repairs",
-                        style = MaterialTheme.typography.labelSmall
-                    )
-                }
+            if (expanded) {
+                ExpandedOptions()
+            }
+            Row {
+                Spacer(modifier = Modifier.weight(1f))
+                ItemButton(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    expanded = expanded,
+                    onClick = { expanded = !expanded },
+                    modifier = modifier
+                )
+                Spacer(modifier = Modifier.weight(1f))
             }
         }
     }
@@ -119,7 +133,7 @@ fun CarItem(modifier: Modifier = Modifier) {
 fun AddCar(modifier: Modifier = Modifier) {
     Card(
         shape = Shapes.medium,
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(top = 0.dp, start = 10.dp, end = 10.dp, bottom = 10.dp)
     ) {
@@ -128,28 +142,69 @@ fun AddCar(modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Add New Car",
+                text = stringResource(id = R.string.add_new_car),
                 style = MaterialTheme.typography.labelSmall,
                 fontSize = 20.sp,
-                modifier = Modifier.padding(top = 5.dp)
+                modifier = modifier.padding(top = 5.dp)
             )
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center
             ) {
                 Spacer(modifier = Modifier.weight(1f))
-                IconButton(
+                ItemButton(
+                    imageVector = Icons.Filled.Add,
                     onClick = { /*TODO*/ },
                     modifier = modifier
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Add,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.secondary
-                    )
-                }
+                )
                 Spacer(modifier = Modifier.weight(1f))
             }
+        }
+    }
+}
+
+@Composable
+fun ItemButton(
+    imageVector: ImageVector,
+    expanded: Boolean = false,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+
+@Composable
+fun ExpandedOptions(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(0.dp, 10.dp)
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text = stringResource(id = R.string.active),
+                style = MaterialTheme.typography.labelSmall,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                text = stringResource(id = R.string.repairs),
+                style = MaterialTheme.typography.labelSmall,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
